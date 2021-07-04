2baa05ef12a220877b744bcb149a4a3d981942dd
idorrington92 <idorrington92@gmail.com>
2021-07-04 19:26:51 +0100

    First iteration of full game. Have looping menu, two game types, scoring system, input checking, and a general framework for each game type that can probably be factored out into an abstract class

--- main.py
+++ main.py
@@ -1,46 +1,24 @@
-import numpy.random as rand
+from multiplyBy11 import MultiplyBy11
+from TwoDigitAddition import TwoDigitAddition
 
-class multiplyBy11:
-    def __init__(self, number_of_rounds):
-        self.number = 0
-        self.player_answer = 0
-        self.number_of_rounds = number_of_rounds
 
-        self.playGame()
+def menu():
+    nrounds = 3
+    selection = input("Select skill to practice:\n"
+                      "1. Multiply by 11\n"
+                      "2. Two digit addition\n"
+                      "q. Quit\n")
 
-    def playGame(self):
-        for round in range(self.number_of_rounds):
-            print(f"Round {round}")
-            if not self.gameRound():
-                break
+    if selection == "1":
+        return MultiplyBy11(nrounds)
+    if selection == "2":
+        return TwoDigitAddition(nrounds)
+    if selection in ('q', 'Q'):
+        return False
 
-    def gameRound(self):
-        self.number = rand.randint(10, 99)
-        self.player_answer = input(f"{self.number} x 11 = ")
-
-        while not self.validInput():
-            print("Invalid input. Please enter numeric value to play or Q to quit\n")
-            self.player_answer = input(f"{self.number} x 11 = ")
-
-        if self.player_answer in ("q", "Q"):
-            print("Exiting program")
-            return False
-
-        if self.playerCorrect():
-            print("Correct!")
-            return True
-
-        print(f"Unlucky. The correct answer is {self.correctAnswer()}")
-        return True
-
-    def correctAnswer(self):
-        return self.number * 11
-
-    def playerCorrect(self):
-        return self.player_answer == str(self.correctAnswer())
-
-    def validInput(self):
-        return self.player_answer in ("q", "Q") or self.player_answer.isnumeric()
 
 if __name__ == '__main__':
-    multiplyBy11(3)
+    while menu():
+        pass
+
+    print("Thank you for playing")
