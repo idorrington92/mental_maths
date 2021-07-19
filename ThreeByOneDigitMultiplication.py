c0c6c3d00738e745870706f01980cd0a83de5733
idorrington92 <idorrington92@gmail.com>
2021-07-19 20:48:52 +0100

    Added new three by one digit multiplication level. Menu also loops until valid entry in input now

---
+++ ThreeByOneDigitMultiplication.py
@@ -0,0 +1,22 @@
+import numpy.random as rand
+
+from Game import Game
+
+
+class ThreeByOneDigitMultiplication(Game):
+    def __init__(self, number_of_rounds):
+        super().__init__(number_of_rounds)
+        self.number1 = 0
+        self.number2 = 0
+        self.help = "..."
+
+        self.play_game()
+
+    def game_round(self):
+        self.number1 = rand.randint(10, 1000)
+        self.number2 = rand.randint(1, 10)
+        self.prompt = f"{self.number1} x {self.number2} = "
+        return self.player_input()
+
+    def correct_answer(self):
+        return self.number1 * self.number2
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from multiplyBy11 import MultiplyBy11
 from TwoDigitAddition import TwoDigitAddition
+from ThreeByOneDigitMultiplication import ThreeByOneDigitMultiplication
+from TwoDigitMultiplication import TwoDigitMultiplication
 
 
 def menu():
@@ -7,14 +9,23 @@ def menu():
     selection = input("Select skill to practice:\n"
                       "1. Multiply by 11\n"
                       "2. Two digit addition\n"
+                      "3. Three by one digit multiplication\n"
+                      "4. Two digit multiplication\n"
                       "q. Quit\n")
 
     if selection == "1":
         return MultiplyBy11(nrounds)
     if selection == "2":
         return TwoDigitAddition(nrounds)
+    if selection == "3":
+        return ThreeByOneDigitMultiplication(nrounds)
+    if selection == "4":
+        return TwoDigitMultiplication(nrounds)
     if selection in ('q', 'Q'):
         return False
+
+    print("Invalid entry\n")
+    return True
 
 
 if __name__ == '__main__':
