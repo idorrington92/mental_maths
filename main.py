450f96d132c628980ccfe1ae0e520102398d712a
idorrington92 <idorrington92@gmail.com>
2021-08-03 20:56:31 +0100

    Started to make the game more of an app by introducing kivy. Now has a basic graphical menu which launches each level (though the levels still play in the command line

--- main.py
+++ main.py
@@ -1,35 +1,70 @@
+from functools import partial
+
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRoundFlatButton
+
 from multiplyBy11 import MultiplyBy11
 from TwoDigitAddition import TwoDigitAddition
 from ThreeByOneDigitMultiplication import ThreeByOneDigitMultiplication
 from TwoDigitMultiplication import TwoDigitMultiplication
 
 
-def menu():
-    nrounds = 3
-    selection = input("Select skill to practice:\n"
-                      "1. Multiply by 11\n"
-                      "2. Two digit addition\n"
-                      "3. Three by one digit multiplication\n"
-                      "4. Two digit multiplication\n"
-                      "q. Quit\n")
+class MentalMathsApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Cyan"
 
-    if selection == "1":
-        return MultiplyBy11(nrounds)
-    if selection == "2":
-        return TwoDigitAddition(nrounds)
-    if selection == "3":
-        return ThreeByOneDigitMultiplication(nrounds)
-    if selection == "4":
-        return TwoDigitMultiplication(nrounds)
-    if selection in ('q', 'Q'):
-        return False
 
-    print("Invalid entry\n")
-    return True
+
+        return self.menu()
+
+    def printIDs(self):
+        print(self.root.ids)
+
+    def menu(self):
+        nrounds = 3
+        screen = MDScreen()
+        btn1 = MDRoundFlatButton(text='Multiply by 11', pos_hint={'center_x': 0.5, 'center_y': 0.8},
+                                 text_color=MDApp.get_running_app().theme_cls.primary_light,
+                                 on_release=partial(MultiplyBy11, nrounds)
+                                 )
+        screen.add_widget(btn1)
+        btn2 = MDRoundFlatButton(text='Two digit addition', pos_hint={'center_x': 0.5, 'center_y': 0.6},
+                                 text_color=MDApp.get_running_app().theme_cls.primary_light,
+                                 on_release=partial(TwoDigitAddition, nrounds)
+                                 )
+        screen.add_widget(btn2)
+        btn3 = MDRoundFlatButton(text='Three by one digit multiplication', pos_hint={'center_x': 0.5, 'center_y': 0.4},
+                                 text_color=MDApp.get_running_app().theme_cls.primary_light,
+                                 on_release=partial(ThreeByOneDigitMultiplication, nrounds)
+                                 )
+        screen.add_widget(btn3)
+        btn4 = MDRoundFlatButton(text='Two digit multiplication', pos_hint={'center_x': 0.5, 'center_y': 0.2},
+                                 text_color=MDApp.get_running_app().theme_cls.primary_light,
+                                 on_release=partial(TwoDigitMultiplication, nrounds)
+                                 )
+        screen.add_widget(btn4)
+        """
+        
+        selection = input("Select skill to practice:\n"
+                          "1. Multiply by 11\n"
+                          "2. Two digit addition\n"
+                          "3. Three by one digit multiplication\n"
+                          "4. Two digit multiplication\n"
+                          )
+
+        if selection == "1":
+            MultiplyBy11(nrounds)
+        if selection == "2":
+            TwoDigitAddition(nrounds)
+        if selection == "3":
+            ThreeByOneDigitMultiplication(nrounds)
+        if selection == "4":
+            TwoDigitMultiplication(nrounds)
+        """
+        return screen
 
 
 if __name__ == '__main__':
-    while menu():
-        pass
-
-    print("Thank you for playing")
+    MentalMathsApp().run()
