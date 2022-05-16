cfeed85ea59f6759691822d6d259b780b3588850
idorrington92 <idorrington92@gmail.com>
2022-05-16 19:05:45 +0100

    Starting to change this game type to be answer questions as quickly as possible, rather than have time limit (WIP)

--- TimedQuiz.py
+++ TimedQuiz.py
@@ -4,26 +4,15 @@ from Game import GameLogic
 
 
 class TimedQuiz(GameLogic):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.number_of_rounds = 3
-        self.g_round = 0
-
     def play_game(self):
         super().play_game()
-        self.g_round = 0
         self.start_round()
 
     def start_round(self, *args):
         MDApp.get_running_app().root.ids["game_screen"].ids.PlayerInput.focus = True
         MDApp.get_running_app().root.ids["game_screen"].ids.PlayerInput.text = self.player_answer = ''
-        self.g_round += 1
-        if self.g_round <= self.number_of_rounds:
-            print(f"Round {self.g_round}")
-            self.quiz.game_round()
-            self.set_prompt(self.quiz.question)
-        else:
-            self.end_game()  # Player reaches end of game without quiting
+        self.quiz.game_round()
+        self.set_prompt(self.quiz.question)
 
     def end_game(self):
         """
@@ -31,14 +20,18 @@ class TimedQuiz(GameLogic):
         :return:
         """
         MDApp.get_running_app().root.ids["game_screen"].ids.PlayerInput.focus = False
-        if self.score / self.number_of_rounds < 0.7:
-            self.EndGamePopUpTitle = "Practice makes perfect"
-        else:
-            self.EndGamePopUpTitle = "Congratulations"
+        self.EndGamePopUpTitle = "Game over"
         super().end_game()
 
     def end_game_text(self):
         return f"\nScore: {self.score}\n" \
-               f"Percent correct: {self.score / self.number_of_rounds * 100:.2f}%\n" \
                f"Time taken: {self.timestep:.2f}s"
 
+    def correct_answer_action(self):
+        super().correct_answer_action()
+        if self.score >= 3:
+            self.end_game()
+
+    def incorrect_answer_action(self):
+        super().incorrect_answer_action()
+        self.timestep += 5
