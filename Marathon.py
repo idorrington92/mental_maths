12616a95264c520d4e00e5b54e0ce0b2b0e4cd20
idorrington92 <idorrington92@gmail.com>
2022-07-17 13:08:28 +0100

    Clock behaviour is correct now (doesn't go negative, answer given as incorrect when time runs out). As is losing lives

--- Marathon.py
+++ Marathon.py
@@ -10,8 +10,9 @@ class Marathon(GameLogic, ABC):
     def __init__(self, *args):
         super().__init__(*args)
         self.clock = None
-        self.initial_time_limit = 20
+        self.initial_time_limit = 5
         self.time_limit = self.initial_time_limit
+        self.clock_paused = False
         # TODO Add lives on game screen
         self.lives = 3
         self.timestep = self.time_limit
@@ -26,9 +27,10 @@ class Marathon(GameLogic, ABC):
         # Time limit per question should slowly decrease
         if not self.question_number % 5:
             self.time_limit *= 0.7
-        self.timestep = self.time_limit
         self.quiz.game_round()
         self.set_prompt(self.quiz.question)
+        self.timestep = self.time_limit
+        self.clock_paused = False
 
     def play_game(self):
         MDApp.get_running_app().root.ids["game_screen"].ids.score.text = f"Score: {self.score}"
@@ -37,16 +39,25 @@ class Marathon(GameLogic, ABC):
         self.start_round()
 
     def reset(self):
+        self.time_limit = self.initial_time_limit
         self.timestep = self.time_limit
+        self.lives = 3
         super().reset()
 
     def update(self, *args):
+        self.timestep -= self.timestep_size
         if self.timestep > 0:
-            self.timestep -= self.timestep_size
             MDApp.get_running_app().root.ids["game_screen"].ids['clock_label'].text = f"{self.timestep:.3f}"
         else:
             # Player runs out of time
-            self.incorrect_answer_action()
+            # Clock only displays zero rather than a negative time
+            MDApp.get_running_app().root.ids["game_screen"].ids['clock_label'].text = f"{0:.3f}"
+            if not self.clock_paused:
+                # When the clock reaches zero, it is paused
+                # NOTE, this is only the display, the actual clock keeps going
+                self.clock_paused = True
+                # The player input should be handled only once (so they don't lose a life with every timestep)
+                self.player_input()
 
     def end_game_text(self):
         return f"Score: {self.score}"
@@ -56,13 +67,14 @@ class Marathon(GameLogic, ABC):
         self.combo = 0
         self.lives -= 1
         # Game ends when player gets an incorrect answer while out of lives
+        print(f"{self.lives=}")
         if self.lives < 0:
             self.end_game()
 
     def correct_answer_action(self):
         super().correct_answer_action()
         self.combo += 1
-        if self.combo % 10 == 1 and self.lives < 3:
+        if not self.combo % 3 and self.lives < 3:
             self.add_life()
         self.score += self.calculate_score()
 
