13260a86a12cddeb7c6b260606e44bc4ee31af41
idorrington92 <idorrington92@gmail.com>
2023-05-02 15:21:43 +0100

    Most of the work on the multiply by 11 tutorial is done. Just need to fix end game now

--- Tutorial.py
+++ Tutorial.py
@@ -1,16 +1,62 @@
 from kivymd.app import MDApp
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+import numpy.random as rand
 
 from Game import GameLogic
+
+
+class TutorialCloseButton(MDFlatButton):
+    pass
+
+
+class TutorialNextButton(MDFlatButton):
+    pass
 
 
 class Tutorial(GameLogic):
     short_description = "Learn how to solve the problem"
 
+    def __init__(self, quiz_logic):
+        super(Tutorial, self).__init__(quiz_logic)
+        self.tutorial_pop_up = None
+        self.tutorial_idx = 0
+        self.tutorial_phase = -1
+
+    def start_countdown(self):
+        # No Countdown for tutorial
+        self.reset()
+        self.app.change_screen("game_screen")
+        self.display_tutorial()
+
     def get_records(self):
         return
 
-    def open_pop_ups(self, score=None):
-        print("TODO custom pop up for tutorials")
+    def update_tutorial(self):
+        self.tutorial_idx += 1
+        # None indicates end of this part of the tutorial
+        if self.tutorial_idx >= len(self.quiz.tutorial):
+            self.end_game()
+        elif self.quiz.tutorial[self.tutorial_idx] is None:
+            self.score = 0
+            self.tutorial_idx += 1
+            self.tutorial_phase += 1
+            self.tutorial_pop_up.dismiss()
+            self.play_game()
+        else:
+            self.tutorial_pop_up.text = self.quiz.tutorial[self.tutorial_idx]
+
+    def generate_tutorial_pop_up(self):
+        self.tutorial_pop_up = MDDialog(title=self.app.quiz_name,
+                                        text=self.quiz.tutorial[self.tutorial_idx],
+                                        buttons=[TutorialNextButton()],
+                                        md_bg_color=self.app.theme_cls.bg_dark,
+                                        auto_dismiss=False,
+                                        )
+
+    def display_tutorial(self):
+        self.generate_tutorial_pop_up()
+        self.tutorial_pop_up.open()
 
     def play_game(self):
         self.app.root.ids["game_screen"].ids.score.text = f"Score: {self.score}"
@@ -20,7 +66,7 @@ class Tutorial(GameLogic):
     def start_round(self, *args):
         MDApp.get_running_app().root.ids["game_screen"].ids.PlayerInput.focus = True
         MDApp.get_running_app().root.ids["game_screen"].ids.PlayerInput.text = self.player_answer = ''
-        self.quiz.game_round()
+        self.quiz.tutorial_game_round(self.tutorial_phase)
         self.set_prompt(self.quiz.question)
 
     def end_game(self):
@@ -29,10 +75,7 @@ class Tutorial(GameLogic):
         :return:
         """
         MDApp.get_running_app().root.ids["game_screen"].ids.PlayerInput.focus = False
-        if self.score < 5:
-            self.EndGamePopUpTitle = "Practice makes perfect"
-        else:
-            self.EndGamePopUpTitle = "Congratulations"
+        self.EndGamePopUpTitle = "Congratulations, you finished the tutorial"
         super().end_game()
 
     def end_game_text(self):
@@ -40,8 +83,10 @@ class Tutorial(GameLogic):
 
     def correct_answer_action(self):
         self.score += 1
+        if self.score >= 3:
+            print(self.tutorial_idx, len(self.quiz.tutorial))
+            if self.tutorial_idx < len(self.quiz.tutorial):
+                self.display_tutorial()
+            else:
+                self.end_game()
         return super().correct_answer_action()
-
-    def incorrect_answer_action(self):
-        super().incorrect_answer_action()
-        self.end_game()
--- multiplyBy11.py
+++ multiplyBy11.py
@@ -13,10 +13,34 @@ class MultiplyBy11(QuizLogic):
                     "the two digits. e.g. 42 x 11 = 462, as 4 + 2 = 6. If the two digits sum to more than 10, then \n" \
                     "add one to the first digit and put the second digit in the middle. e.g. 58 x 11 = 638 as 5 + 8\n" \
                     " = 13, so we add one to the 5 and put 3 in the middle."
+        self.tutorial = ["We are going to learn a quick and easy way to multiply a two digit number by 11",
+                         "The trick is to add the two digits together, and sandwich the result between them,"
+                         "so 42x11 become 462, as 4+2=6 and we put this between the 4 and 2",
+                         "Now you try. Get three correct answers to move to the next section",
+                         None,
+                         "Well done! You got three correct answers!",
+                         "You may be wondering what to do when the two digits add up to more than 10, such as in 67x11.",
+                         "In this case you add the two digits together, and sandwich the"
+                         " second digit of the answer between the two digits, and add one to the first digit.\n"
+                         " So 67x11=737, as we sandwich the second digit of 6+7=13 and add one to the first digit.",
+                         "Now you have a go! Get three correct answers to move on!",
+                         None,
+                         "Well done! You completed the tutorial!"
+                        ]
 
     def game_round(self):
         self.number = rand.randint(10, 100)
         self.question = f"{self.number} x 11 = "
+
+    def tutorial_game_round(self, tutorial_phase):
+        if tutorial_phase == 0:
+            numbers = [i for i in range(10, 100) if int(str(i)[0]) + int(str(i)[1]) < 10]
+            self.number = rand.choice(numbers)
+        else:
+            numbers = [i for i in range(10, 100) if int(str(i)[0]) + int(str(i)[1]) >= 10]
+            self.number = rand.choice(numbers)
+        self.question = f"{self.number} x 11 = "
+
 
     def correct_answer(self):
         return self.number * 11
